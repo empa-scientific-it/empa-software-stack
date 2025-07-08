0f8152bc055cd153ddb0784d7e980426b60adbcd
Edoardo Baldi <edoardob90@gmail.com>
2025-07-08 15:02:58 +0200

    Prepend local, arch-based path to LD_LIBRARY_PATH

--- modules/Ansys/2024_R2.lua
+++ modules/Ansys/2024_R2.lua
@@ -17,12 +17,25 @@ local bin = pathJoin(root, "ansys/bin")
 
 conflict("Ansys")
 
+-- Detect architecture
+local arch = capture("uname -m"):gsub("\n", "")
+
+-- Construct path to user-local libs
+local user_lib64 = pathJoin(os.getenv("HOME"), ".local", arch, "lib64")
+local user_lib   = pathJoin(os.getenv("HOME"), ".local", arch, "lib")
+
+-- Prepend to LD_LIBRARY_PATH
+prepend_path("LD_LIBRARY_PATH", user_lib64)
+prepend_path("LD_LIBRARY_PATH", user_lib)
+
+-- Ansys root
 prepend_path("PATH", bin)
 setenv("ANSYS_ROOT", root)
 setenv("ANSYS_BIN", bin)
 setenv("AWP_ROOT242", root)
 setenv("ANSYS_VERSION", "2024_R2_v242")
 setenv("ANSYSLMD_LICENSE_FILE", "1055@ansyslic.empa.ch")
+
 -- Fluent variables
 prepend_path("PATH", pathJoin(root, "fluent/bin"))
 setenv("FLUENT_INC", pathJoin(root, "fluent"))
