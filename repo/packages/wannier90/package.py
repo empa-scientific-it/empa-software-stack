749b1114089ceaf03255fd10c25dbc99b331f4af
AndresOrtegaGuerrero <oandresg15@gmail.com>
2025-08-21 13:05:52 +0200

    edoardob changes

--- repo/packages/wannier90/package.py
+++ repo/packages/wannier90/package.py
@@ -14,6 +14,12 @@ class Wannier90(MakefilePackage):
 
     license("GPL-3.0-or-later", checked_by="AndresOrtegaGuerrero")
 
+    version("develop", branch="develop")
+    version("3.1.0", sha256="40651a9832eb93dec20a8360dd535262c261c34e13c41b6755fa6915c936b254")
+    version("3.0.0", sha256="f196e441dcd7b67159a1d09d2d7de2893b011a9f03aab6b30c4703ecbf20fe5b")
+    version("2.1.0", sha256="ee90108d4bc4aa6a1cf16d72abebcb3087cf6c1007d22dda269eb7e7076bddca")
+    version("2.0.1", sha256="05ea7cd421a219ce19d379ad6ae3d9b1a84be4ffb367506ffdfab1e729309e94")
+    
     # Dependencies 
 
     depends_on("fortran")
