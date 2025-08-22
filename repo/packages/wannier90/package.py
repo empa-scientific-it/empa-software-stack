8bdfd76b933cc89271a0631d8aab546e67c27c57
Edoardo Baldi <edoardob90@gmail.com>
2025-08-22 10:22:27 +0200

    Reformat

--- repo/packages/wannier90/package.py
+++ repo/packages/wannier90/package.py
@@ -15,12 +15,24 @@ class Wannier90(MakefilePackage):
     license("GPL-3.0-or-later", checked_by="AndresOrtegaGuerrero")
 
     version("develop", branch="develop")
-    version("3.1.0", sha256="40651a9832eb93dec20a8360dd535262c261c34e13c41b6755fa6915c936b254")
-    version("3.0.0", sha256="f196e441dcd7b67159a1d09d2d7de2893b011a9f03aab6b30c4703ecbf20fe5b")
-    version("2.1.0", sha256="ee90108d4bc4aa6a1cf16d72abebcb3087cf6c1007d22dda269eb7e7076bddca")
-    version("2.0.1", sha256="05ea7cd421a219ce19d379ad6ae3d9b1a84be4ffb367506ffdfab1e729309e94")
-    
-    # Dependencies 
+    version(
+        "3.1.0",
+        sha256="40651a9832eb93dec20a8360dd535262c261c34e13c41b6755fa6915c936b254",
+    )
+    version(
+        "3.0.0",
+        sha256="f196e441dcd7b67159a1d09d2d7de2893b011a9f03aab6b30c4703ecbf20fe5b",
+    )
+    version(
+        "2.1.0",
+        sha256="ee90108d4bc4aa6a1cf16d72abebcb3087cf6c1007d22dda269eb7e7076bddca",
+    )
+    version(
+        "2.0.1",
+        sha256="05ea7cd421a219ce19d379ad6ae3d9b1a84be4ffb367506ffdfab1e729309e94",
+    )
+
+    # Dependencies
 
     depends_on("fortran")
     depends_on("mpi")
@@ -28,9 +40,7 @@ class Wannier90(MakefilePackage):
 
     variant("mpi", default=True, description="Enable MPI support")
 
-
     def setup_build_environment(self, env):
-
         if "+mpi" in self.spec:
             env.set("COMMS", "mpi")
             env.set("MPIF90", self.spec.mpifc)
@@ -41,17 +51,13 @@ class Wannier90(MakefilePackage):
 
         libs = self.spec["openblas"].libs
         env.set("LIBS", libs.ld_flags)
-        
+
         env.append_flags("FFLAGS", "-O3 -fPIC -g -fallow-argument-mismatch")
         env.append_flags("LDFLAGS", "-O3 -fPIC")
 
     def install(self, spec, prefix):
-
         if "+mpi" in spec:
             make("lib", "COMMS=mpi")
 
         # Only wannier90.x and postw90.x
         make("default")
-
-
-
